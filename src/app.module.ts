c1a154929407af481e7dc5b2d856d25b1e7e5586
ProVitSer <prokinvs@gmail.com>
2022-11-26 12:47:25 +0300

    cdr queue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CdrModule } from './cdr/cdr.module';
+import configuration from './config/config.provider';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration] }),
+    CdrModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const { username, password, database, host } =
+          configService.get('mongo');
+
+        return {
+          uri: `mongodb://${username}:${password}@${host}/${database}`,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [ConfigModule],
 })
 export class AppModule {}
